7b47c42d200dc46a0b84b4451d8b0f99c9c7e2ad
Wesley Luyten <me@wesleyluyten.com>
2021-06-27 11:48:26 -0500

    Add Streamable meta

--- src/providers/streamable.js
+++ src/providers/streamable.js
@@ -8,6 +8,25 @@ export default {
 
   options: 'maxwidth maxheight',
 
+  scrape: {
+    duration: {
+      selector: 'script[data-duration]',
+      value: (element) => Number(element.getAttribute('data-duration'))
+    },
+    upload_date: {
+      selector: 'meta[property="og:updated_time"]',
+      value: (element) => new Date(element.getAttribute('content')).toISOString()
+    },
+    embed_url: {
+      selector: 'meta[name="twitter:player"]',
+      value: (element) => element.getAttribute('content')
+    },
+    content_url: {
+      selector: 'meta[property="og:video:url"]',
+      value: (element) => element.getAttribute('content')
+    },
+  },
+
   buildUrl(req) {
     let url = new URL('https://api.streamable.com/oembed.json');
     url.searchParams.set('url', req.url);
