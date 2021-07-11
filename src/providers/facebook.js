7011c67a394f6f6aae34c7f4471abf1d199f2bf3
Wesley Luyten <me@wesleyluyten.com>
2021-07-10 19:34:38 -0500

    Update facebook provider

--- src/providers/facebook.js
+++ src/providers/facebook.js
@@ -1,4 +1,4 @@
-/* global FB_ACCESS_TOKEN */
+/* global IFRAMELY_API_KEY */
 export default {
 
   patterns: [
@@ -8,13 +8,36 @@ export default {
 
   name: 'Facebook',
 
-  options: 'maxwidth omitscript',
+  options: 'maxwidth maxheight api_key',
 
   buildUrl(req) {
-    let url = new URL('https://graph.facebook.com/v11.0/oembed_video');
+    // FB removed their public oEmbed API :/
+    let url = new URL('https://iframe.ly/api/iframely');
     url.searchParams.set('url', req.url);
-    url.searchParams.set('access_token', FB_ACCESS_TOKEN);
+    // This is a Iframely dev license limited to 1000 unique hits per month!
+    url.searchParams.set('api_key', IFRAMELY_API_KEY)
     return url;
+  },
+
+  serialize({ meta, links: { player, thumbnail } }) {
+    const { title, description, author, author_url, duration, date } = meta;
+
+    return {
+      type: 'video',
+      version: '1.0',
+      provider_name: 'Facebook',
+      provider_url: 'https://www.facebook.com/',
+      title,
+      description,
+      author_name: author,
+      author_url,
+      duration,
+      upload_date: date,
+      thumbnail_url: thumbnail[0].href,
+      thumbnail_width: thumbnail[0].media.width,
+      thumbnail_height: thumbnail[0].media.height,
+      html: player[0].html,
+    };
   },
 
 };
