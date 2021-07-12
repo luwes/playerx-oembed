e0fc2ea5f3a6be75d0b8faa821296d8a3c61c8b1
Wesley Luyten <me@wesleyluyten.com>
2021-07-11 23:07:33 -0500

    Sort json response

--- src/provider.js
+++ src/provider.js
@@ -36,15 +36,39 @@ export default {
     });
   },
 
-  serialize(body) {
-    return body;
-  },
-
   async finalize(req, data) {
     if (this.scrape) {
       Object.assign(data, await scraper(req.url, this.scrape));
     }
-    return this.serialize(data);
+    return this.sortJson(await this.serialize(data));
   },
+
+  serialize(body) {
+    return body;
+  },
+
+  sortJson(data) {
+    return Object.assign({
+      version: undefined,
+      type: undefined,
+      provider_name: undefined,
+      provider_url: undefined,
+      cache_age: undefined,
+      title: undefined,
+      description: undefined,      // schema ext
+      duration: undefined,         // schema ext
+      upload_date: undefined,      // schema ext
+      author_name: undefined,
+      author_url: undefined,
+      thumbnail_url: undefined,
+      thumbnail_width: undefined,
+      thumbnail_height: undefined,
+      html: undefined,
+      width: undefined,
+      height: undefined,
+      embed_url: undefined,        // schema ext
+      content_url: undefined,      // schema ext
+    }, data);
+  }
 
 }
