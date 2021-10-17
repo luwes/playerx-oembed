1614c234ef4dcb1d20b3481d629042560c722a93
Wesley Luyten <me@wesleyluyten.com>
2021-10-16 19:44:58 -0500

    Fix tests

--- test/facebook.spec.js
+++ test/facebook.spec.js
@@ -26,6 +26,6 @@ test('https://www.facebook.com/wesleyluyten/videos/10220940465559072', async (t)
   )
   t.regex(
     json.html,
-    new RegExp('<div id="fb-root"></div>\\n<script async="1" defer="1" crossorigin="anonymous" src="https://connect.facebook.net/en_US/sdk.js#xfbml=1&amp;version=v11.0" nonce="\\w+"></script><div class="fb-video" data-href="https://www.facebook.com/wesleyluyten/videos/10220940465559072"><blockquote cite="https://www.facebook.com/wesleyluyten/videos/10220940465559072/" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/wesleyluyten/videos/10220940465559072/"></a><p>Was dreaming about butterflies during the week got this in the weekend in Philadelphia on a random city trip!</p>Posted by <a href="https://www.facebook.com/wesleyluyten">Wesley Luyten</a> on Saturday, February 29, 2020</blockquote></div>')
+    new RegExp('<div id="fb-root"></div>\\n<script async="1" defer="1" crossorigin="anonymous" src="https://connect.facebook.net/en_US/sdk.js#xfbml=1&amp;version=v12.0" nonce="\\w+"></script><div class="fb-video" data-href="https://www.facebook.com/wesleyluyten/videos/10220940465559072"><blockquote cite="https://www.facebook.com/wesleyluyten/videos/10220940465559072/" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/wesleyluyten/videos/10220940465559072/"></a><p>Was dreaming about butterflies during the week got this in the weekend in Philadelphia on a random city trip!</p>Posted by <a href="https://www.facebook.com/wesleyluyten">Wesley Luyten</a> on Saturday, February 29, 2020</blockquote></div>')
   )
 })
